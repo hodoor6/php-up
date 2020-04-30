fd989694ecb71c66bfa68e866955b6b398e44f04
Aleksey Klimchuk <hodoor6@yandex.ua>
2020-04-30 12:07:44 +0300

    1. Планировщик задач.
    а) Написать скрипт, который получает текущее время (например, 2017-01-10 18:05:24) и записывает его c новой строки в конец файла /var/www/project/frontend/web/log.txt
    Таким образом, каждый раз, когда вызывается данный скрипт, в файл log.txt будет записано время его вызова.
    
    б) Настроить планировщик на повторение скрипта с частотой:
    
        Каждую минуту
        Каждый час
        Раз в 5 минут
        Раз в 10 минут
    
    в) Оставить машину рабочей и проверить содержимое файла log.txt спустя некоторое время.
    
    Подсказка: можно сделать вывод времени немного красивее используя встроенный компонент Yii::$app->formatter.

--- console/controllers/TimeController.php
+++ console/controllers/TimeController.php
@@ -9,7 +9,16 @@ class TimeController extends \yii\console\Controller
 
     public function actionGetTime()
     {
-        echo 'Время отработки скрипта 1 минута в cron' . date("Y-d-m G:i:s");
-        file_put_contents('test.txt',1);
+
+        $currentTime  =' Время отработки скрипта 1 минута в cron ' . date("Y-d-m G:i:s");
+
+        $file = file_get_contents("/var/www/project/frontend/web/log.txt", ' ');
+
+
+        file_put_contents('/var/www/project/frontend/web/log.txt',
+            $file . $currentTime . PHP_EOL );
+//        $file = fopen("/var/www/project/frontend/web/log.txt", 'a+');
+//        fwrite($file,$currentTime. PHP_EOL);
+//        fclose($file);
     }
 }
